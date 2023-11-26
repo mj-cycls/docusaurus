543358b274981faae8678e516a3cb4330f567bec
Mohammed Alrujayi <58256600+mj-cycls@users.noreply.github.com>
2023-11-27 00:38:13 +0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,6 +79,11 @@ const config = {
           // },
           // { to: "/blog", label: "Blog", position: "left" },
           {
+            href: 'https://discord.gg/BMnaMatDC7',
+            label: 'Discord',
+            position: 'right',
+          },
+          {
             href: `https://github.com/${organizationName}/${projectName}`,
             label: "GitHub",
             position: "right",
